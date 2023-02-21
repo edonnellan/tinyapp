a5e2c5c93071c34a170107ce526d3cd60b22acf8
EDonnellan <elaine.donny@gmail.com>
2023-02-21 21:05:00 +0000

    fixed the /urls/new to redirect to urls_new. All's good and working atm.

--- express_server.js
+++ express_server.js
@@ -6,17 +6,20 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 const generateRandomString = () => {
-  return Math.random().toString(32).slice(7);
-}
+  return Math.random().toString(36).slice(7);
+};
 
 app.post("/urls", (req, res) => {
   console.log(req.body);
-  res.send("OK");
+  const tinyUrl = generateRandomString();
+  urlDatabase[tinyUrl] = req.body.longURL;
+  console.log("urlsDatabase: ", urlDatabase, "tinyUrl: ", tinyUrl);
+  res.redirect(`/urls/${tinyUrl}`);
 });
 
 app.get("/urls", (req, res) => {
@@ -24,16 +27,25 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls_new", (req, res) => {
+app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  // console.log("id: ", req.params.id);
+  res.redirect(longURL);
+});
+
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
   res.render("urls_show", templateVars);
 });
 
-app.get("/" ,(req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
